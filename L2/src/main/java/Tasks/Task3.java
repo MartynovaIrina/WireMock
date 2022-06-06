9ced9830f00bb0f6d58032059caf24a0d8ebe0ad
Irina <irinamartynova92@gmail.com>
2022-06-06 11:32:07 +0300

    Second commit, vars relocated Task_3

--- L2/src/main/java/Tasks/Task3.java
+++ L2/src/main/java/Tasks/Task3.java
@@ -21,26 +21,35 @@ public class Task3 {
         String[] cities = new String[]{"Gold Coast" , "Palikir" , "Toronto"};
         String[] streets = new String[]{"Surfers Paradise" , "National Government" , "Saint Patrick"};
 
+        int priceApple;
+        int weightApple;
+        int priceOrange;
+        int weightOrange;
+        boolean withBones;
+        int building;
+        int floor;
+        int apartment;
+        int random;
         for (int i = 1; i <= numberOfOrders; i++) {
             List<Product> products = new ArrayList<>();
-            int priceApple = (int)(Math.random() * 600 + 1);
-            int weightApple = (int)(Math.random() * 200 + 1);
+            priceApple = (int)(Math.random() * 600 + 1);
+            weightApple = (int)(Math.random() * 200 + 1);
             Apple apple = new Apple(priceApple, "apple", weightApple, "red");
             if (i % 2 == 0) {
                 products.add(apple);
             }
             else {
-                int priceOrange = (int)(Math.random() * 600 + 1);
-                int weightOrange = (int)(Math.random() * 200 + 1);
-                boolean withBones = (Math.random() < 0.5);
+                priceOrange = (int)(Math.random() * 600 + 1);
+                weightOrange = (int)(Math.random() * 200 + 1);
+                withBones = (Math.random() < 0.5);
                 Orange orange = new Orange(priceOrange, "orange", weightOrange, withBones);
                 products.add(apple);
                 products.add(orange);
             }
-            int building = (int)(Math.random() * 20 + 1);
-            int floor = (int)(Math.random() * 15 + 1);
-            int apartment = (int)(Math.random() * 200 + 1);
-            int random = (int)(Math.random() * streets.length);
+            building = (int)(Math.random() * 20 + 1);
+            floor = (int)(Math.random() * 15 + 1);
+            apartment = (int)(Math.random() * 200 + 1);
+            random = (int)(Math.random() * streets.length);
             Address address = new Address(countries[random],
                     cities[random], streets[random], building, floor, apartment);
             Order orderCurr = new Order(products, address, i);
